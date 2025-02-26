f94c31bd60e33ddfeb1e5b707f4ee1bea8db399d
Bruno M <c.brunomelo@outlook.com>
2025-02-26 19:14:16 -0300

    Concluido Questao dois

--- TargetSistemas/Program.cs
+++ TargetSistemas/Program.cs
@@ -4,3 +4,6 @@
 int valor = SomaValores.Calcular();
 Console.WriteLine($"Questão 1: {valor}");
 
+// Questão 2
+bool pertence = Fibonacci.PertenceASequencia(13);
+Console.WriteLine($"Questão 2: O número 13 pertence à sequência de Fibonacci? {pertence}");
---
+++ TargetSistemas/Questoes/Fibonacci.cs
@@ -0,0 +1,18 @@
+﻿namespace TargetSistemas.Questoes;
+
+internal class Fibonacci
+{
+    public static bool PertenceASequencia(int numero)
+    {
+        return PertenceASequenciaRecursiva(numero, 0, 1);
+    }
+
+    private static bool PertenceASequenciaRecursiva(int numero, int a, int b)
+    {
+        if (a == numero || b == numero)
+            return true;
+        if (b > numero)
+            return false;
+        return PertenceASequenciaRecursiva(numero, b, a + b);
+    }
+}
