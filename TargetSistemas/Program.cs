da9e04be755c7fafc26f976fe8aa10d09faa25a7
Bruno M <c.brunomelo@outlook.com>
2025-02-26 20:07:01 -0300

    Concluido Questao 3

--- TargetSistemas/Program.cs
+++ TargetSistemas/Program.cs
@@ -1,4 +1,5 @@
 ﻿using TargetSistemas.Questoes;
+using static TargetSistemas.Questoes.FaturamentoDistribuidora;
 
 // Questão 1
 int valor = SomaValores.Calcular();
@@ -7,3 +8,32 @@ Console.WriteLine($"Questão 1: {valor}");
 // Questão 2
 bool pertence = Fibonacci.PertenceASequencia(13);
 Console.WriteLine($"Questão 2: O número 13 pertence à sequência de Fibonacci? {pertence}");
+
+
+// Questão 3
+//• O menor valor de faturamento ocorrido em um dia do mês;
+
+FaturamentoDistribuidora faturamento = new FaturamentoDistribuidora();
+
+Faturamento menorFaturamento = faturamento.MenorValor();
+
+Console.WriteLine($"Questão 3: O menor valor de faturamento ocorrido em um dia do mês foi de {menorFaturamento.Valor} no dia {menorFaturamento.Dia}.");
+
+//• O maior valor de faturamento ocorrido em um dia do mês;
+
+Faturamento maiorFaturamento = faturamento.MaiorValor();
+
+Console.WriteLine($"Questão 3: O maior valor de faturamento ocorrido em um dia do mês foi de {maiorFaturamento.Valor} no dia {maiorFaturamento.Dia}.");
+
+
+// • Número de dias no mês em que o valor de faturamento diário foi superior à média mensal.
+
+List<Faturamento> diasAcimaDaMedia = faturamento.DiasAcimaDaMedia();
+
+Console.WriteLine($"Questão 3: O número de dias no mês em que o valor de faturamento diário foi superior à média mensal foi de {diasAcimaDaMedia.Count}.");
+
+foreach (Faturamento dia in diasAcimaDaMedia)
+{
+    Console.WriteLine($"Dia {dia.Dia}: {dia.Valor}");
+}
+
---
+++ TargetSistemas/Questoes/FaturamentoDistribuidora.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace TargetSistemas.Questoes;
+
+internal class FaturamentoDistribuidora
+{
+    private IReadOnlyList<Faturamento> Faturamentos { get; set; }
+
+    public FaturamentoDistribuidora() => Faturamentos = GetFaturamentos();
+    
+
+    public Faturamento MenorValor() => Faturamentos.Where(f => f.Valor > 0).OrderBy(f => f.Valor).FirstOrDefault();
+    public Faturamento MaiorValor() => Faturamentos.Where(f => f.Valor > 0).OrderByDescending(f => f.Valor).FirstOrDefault();
+    public List<Faturamento> DiasAcimaDaMedia()
+    {
+        decimal media = Faturamentos.Where(f => f.Valor > 0).Average(f => f.Valor);
+        return Faturamentos.Where(f => f.Valor > media).OrderBy(f => f.Dia).ToList();
+    }
+
+    public record Faturamento([property: JsonPropertyName("dia")] int Dia, [property: JsonPropertyName("valor")] decimal Valor);
+
+    private List<Faturamento> GetFaturamentos()
+    {
+        string projectDirectory = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\.."));
+        string filePath = Path.Combine(projectDirectory, "Dados", "Font1.json");
+
+        if (File.Exists(filePath))
+        {
+            string json = File.ReadAllText(filePath);
+            return JsonSerializer.Deserialize<List<Faturamento>>(json);
+        }
+
+
+        return new List<Faturamento>();
+
+    }
+
+}
